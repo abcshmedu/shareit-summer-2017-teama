a9472858a263da65fffc3e20de8dfcfc4a12cfa6
Maxl Dale <maxl.dale.11@gmail.com>
2017-05-03 15:44:47 +0200

    MockMediaServiceImpl completed with stub methods. TestDiscRestApi and TestBookRestApi completed and functioning.

---
+++ src/test/java/edu/hm/shareit/api/TestBookRestApi.java
@@ -0,0 +1,97 @@
+package edu.hm.shareit.api;
+
+import edu.hm.shareit.models.mediums.Book;
+
+import edu.hm.shareit.models.mediums.Vars;
+import edu.hm.shareit.resources.MediaServiceResult;
+import edu.hm.shareit.resources.MockMediaServiceImpl;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import javax.ws.rs.core.Response;
+
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestBookRestApi extends BookRestApi{
+
+    private static Thread worker;
+
+    private static BookRestApi bookRestApi;
+    private static Response testResponse;
+
+    @BeforeClass
+    public static void setup() {
+        worker = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String APP_URL = "/";
+                int PORT = 8082;
+                String WEBAPP_DIR = "./src/main/webapp/";
+                Server jetty = new Server(PORT);
+                jetty.setHandler(new WebAppContext(WEBAPP_DIR, APP_URL));
+
+                try {
+                    jetty.start();
+                    jetty.join();
+                }catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    jetty.destroy();
+                }
+            }
+        });
+        worker.start();
+        bookRestApi = new BookRestApi();
+        bookRestApi.setMediaService(new MockMediaServiceImpl());
+    }
+
+    @Test
+    public void testGetBook() {
+        testResponse = bookRestApi.getBook(null);
+        assertEquals(null, testResponse.getEntity());
+
+        testResponse = bookRestApi.getBook(Vars.otherIsbn);
+        assertEquals(new Book(), testResponse.getEntity());
+
+        testResponse = bookRestApi.getBook(Vars.isbn);
+        assertEquals(Vars.testBook, testResponse.getEntity());
+    }
+
+    @Test
+    public void testGetBooks() {
+        testResponse = bookRestApi.getBooks();
+        assertEquals(Collections.singletonList(Vars.testBook), testResponse.getEntity());
+    }
+
+    @Test
+    public void testPostBook() {
+        testResponse = bookRestApi.postBook(Vars.testBook);
+        assertEquals(MediaServiceResult.ACCEPTED, testResponse.getEntity());
+    }
+
+    @Test
+    public void testUpdateBook() {
+        testResponse = bookRestApi.updateBook(Vars.testBook, null);
+        assertEquals(MediaServiceResult.ISBN_NOT_FOUND, testResponse.getEntity());
+
+        testResponse = bookRestApi.updateBook(Vars.testBook, Vars.otherIsbn);
+        assertEquals(MediaServiceResult.ISBN_DOES_NOT_MATCH, testResponse.getEntity());
+
+        testResponse = bookRestApi.updateBook(Vars.testBook, Vars.isbn);
+        assertEquals(MediaServiceResult.ACCEPTED, testResponse.getEntity());
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        try{
+            worker.join(100);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/edu/hm/shareit/api/TestDiscRestApi.java
@@ -0,0 +1,97 @@
+package edu.hm.shareit.api;
+
+import edu.hm.shareit.models.mediums.Disc;
+import edu.hm.shareit.models.mediums.Vars;
+import edu.hm.shareit.resources.MediaServiceResult;
+import edu.hm.shareit.resources.MockMediaServiceImpl;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import javax.ws.rs.core.Response;
+
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestDiscRestApi extends DiscRestApi{
+
+    private static Thread worker;
+
+    private static DiscRestApi discRestApi;
+    private static Response testResponse;
+
+    @BeforeClass
+    public static void setup() {
+        worker = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String APP_URL = "/";
+                int PORT = 8082;
+                String WEBAPP_DIR = "./src/main/webapp/";
+                Server jetty = new Server(PORT);
+                jetty.setHandler(new WebAppContext(WEBAPP_DIR, APP_URL));
+
+                try {
+                    jetty.start();
+                    jetty.join();
+                }catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    jetty.destroy();
+                }
+            }
+        });
+        worker.start();
+        discRestApi = new DiscRestApi();
+        discRestApi.setMediaService(new MockMediaServiceImpl());
+    }
+
+    @Test
+    public void testGetDisc() {
+        testResponse = discRestApi.getDisc(null);
+        assertEquals(null, testResponse.getEntity());
+
+        testResponse = discRestApi.getDisc(Vars.otherBarcode);
+        assertEquals(new Disc(), testResponse.getEntity());
+
+        testResponse = discRestApi.getDisc(Vars.barcode);
+        assertEquals(Vars.testDisc, testResponse.getEntity());
+    }
+
+    @Test
+    public void testGetDiscs() {
+        testResponse = discRestApi.getDiscs();
+        assertEquals(Collections.singletonList(Vars.testDisc), testResponse.getEntity());
+    }
+
+    @Test
+    public void testPostBook() {
+        testResponse = discRestApi.postDisc(Vars.testDisc);
+        assertEquals(MediaServiceResult.ACCEPTED, testResponse.getEntity());
+    }
+
+    @Test
+    public void testUpdateBook() {
+        testResponse = discRestApi.updateDisc(Vars.testDisc, null);
+        assertEquals(MediaServiceResult.ISBN_NOT_FOUND, testResponse.getEntity());
+
+        testResponse = discRestApi.updateDisc(Vars.testDisc, Vars.otherBarcode);
+        assertEquals(MediaServiceResult.ISBN_DOES_NOT_MATCH, testResponse.getEntity());
+
+        testResponse = discRestApi.updateDisc(Vars.testDisc, Vars.barcode);
+        assertEquals(MediaServiceResult.ACCEPTED, testResponse.getEntity());
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        try{
+            worker.join(100);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
+
