75374389394de7f98a3502b4cf6cb7f59024e28b
Maxl Dale <maxl.dale.11@gmail.com>
2017-05-03 14:15:15 +0200

    Commented API Classes, fixed faulty constructors.

--- src/main/java/edu/hm/shareit/api/BookRestApi.java
+++ src/main/java/edu/hm/shareit/api/BookRestApi.java
@@ -11,23 +11,22 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Collection;
 
+/**
+ * REST Resource Class for the Book API
+ *
+ * GET  /books/{isbn}   ->  getBook
+ * GET  /books          ->  getBooks
+ * POST /books          ->  postBook
+ * PUT  /books/{isbn}   ->  updateBook
+ */
 @Path("books")
 public class BookRestApi {
-    /**
-     * Field for the backend service
-     */
-    private static MediaService MEDIA_SERVICE;
-
-    /**
-     * Constructor, sets MediaService to default production MediaServiceImpl
-     */
-    public BookRestApi(){
-        setMediaService(new MediaServiceImpl());
-    }
+    // Field for the backend service
+    private static MediaService MEDIA_SERVICE = new MediaServiceImpl();
 
     /**
      * Helper Method, used for setting MediaService (to allow for testing)
-     * @param mediaService the MediaService to use
+     * @param mediaService The MediaService to use
      */
     protected void setMediaService(MediaService mediaService){
         MEDIA_SERVICE = mediaService;
@@ -35,18 +34,21 @@ public class BookRestApi {
 
     /**
      * GET (getBook) Returns a specific book, provided it exists
-     * @param isbn the ISBN for the book to get
-     * @return The Json format for the book
+     * @param isbn The ISBN for the book to get
+     * @return The Response with the Json format for the book
      */
     @GET
     @Path("{isbn}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getBook(@PathParam("isbn") String isbn) {
         Book book = MEDIA_SERVICE.getBook(isbn);
-        System.out.println(book);
         return Response.ok(book).build();
     }
 
+    /**
+     * GET (getBooks) Returns all books
+     * @return The Response with the Json array format for the books
+     */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getBooks() {
@@ -54,19 +56,34 @@ public class BookRestApi {
         return Response.ok(books).build();
     }
 
+    /**
+     * POST (postBook) Posts a book to the Media_Service
+     * @param book The book to post
+     * @return The Response from the MediaService
+     */
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     public Response postBook(Book book) {
         MediaServiceResult result = MEDIA_SERVICE.addBook(book);
-        return Response.ok(result.getStatus()).status(result.getCode()).build();
+        return Response.ok(result.getStatus())
+                .status(result.getCode())
+                .build();
     }
 
+    /**
+     * PUT (updateBook) Updates a book with the given ISBN to the new values
+     * @param book The book with the new values
+     * @param isbn The ISBN for the book to replace
+     * @return The Response from the MediaService
+     */
     @PUT
     @Path("{isbn}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response updateBook(Book book, @PathParam("isbn") String isbn) {
         MediaServiceResult result = MEDIA_SERVICE.updateBook(book, isbn);
-        return Response.ok(result.getStatus()).status(result.getCode()).build();
+        return Response.ok(result.getStatus())
+                .status(result.getCode())
+                .build();
     }
 }
